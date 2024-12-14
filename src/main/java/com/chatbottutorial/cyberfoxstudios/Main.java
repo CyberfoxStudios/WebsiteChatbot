422fedee18e8549545056a31e81b95a94d5f2056
JTMintsASU <64926558+CyberfoxStudios@users.noreply.github.com>
2024-12-14 11:56:21 -0700

    Creates index.html template that includes a bootstrap demo.

--- src/main/java/com/chatbottutorial/cyberfoxstudios/Main.java
+++ src/main/java/com/chatbottutorial/cyberfoxstudios/Main.java
@@ -1,6 +1,10 @@
 package com.chatbottutorial.cyberfoxstudios;
 
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.HashMap;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,7 +16,9 @@ public class Main {
 
         Spark.staticFileLocation("public_html");
         Spark.get("/", (request, response) -> {
-            return "hello world";
+            HashMap<String, Object> model = new HashMap<>();
+            return new ThymeleafTemplateEngine().render(new ModelAndView(model, "index"));
         });
+        //localhost: 127.0.0.1:4567
     }
 }
